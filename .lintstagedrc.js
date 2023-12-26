198f5c88bb80a558fe685c21f9934ac643f6386f
Snowflyt <gaoge011022@163.com>
2023-12-26 15:39:07 +0800

    🐳 chore: Add `.hintrc` to Prettier formatting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,10 +2,11 @@
 
 /** @satisfies {import('lint-staged').Config} */
 const config = {
-  '{src,test}/**/*.{js,ts}': ['eslint --fix'],
-  '*.{js,cjs,mjs,ts,cts,mts}': ['eslint --fix'],
-  '{src,test}/**/*.json': ['prettier --loglevel=silent --write'],
-  '*.{js,cjs,mjs,ts,cts,mts,json,md}': ['prettier --loglevel=silent --write'],
+  '{src,test}/**/*.{js,ts}': 'eslint --fix',
+  '*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix',
+  '{src,test}/**/*.json': 'prettier --loglevel=silent --write',
+  '*.{json,md}': 'prettier --loglevel=silent --write',
+  '.hintrc': 'prettier --loglevel=silent --write --parser json',
 };
 
 export default config;
