5721e4c85d3d6146b8bbfed097bcd666ddf64009
Snowflyt <gaoge011022@163.com>
2023-12-26 16:49:29 +0800

    🐳 chore(commitlint): Update `scope-case` rule in `commitlint.config.js`

--- commitlint.config.js
+++ commitlint.config.js
@@ -82,7 +82,7 @@ const config = {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
     'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'always', 'lower-case'],
+    'scope-case': [2, 'always', ['lower-case', 'upper-case']],
     'subject-case': [2, 'always', 'sentence-case'],
     'subject-empty': [2, 'never'],
     'subject-exclamation-mark': [2, 'never'],
