5bfbfd212be3cab128469e2f674748e99fd354fa
Snowflyt <gaoge011022@163.com>
2025-02-06 19:26:16 +0800

    🐳 chore(example/simple-plugin): Update to meet the latest lib version

--- examples/simple-plugin/src/validators.ts
+++ examples/simple-plugin/src/validators.ts
@@ -1,16 +1,20 @@
-import type { Stringify } from 'typroof';
+import type { Actual, IsNegated, Stringify, Validator } from 'typroof/plugin';
 
 type IsFoo<T> = T extends 'foo' ? true : false;
 
-declare module 'typroof' {
-  interface Validator<T, U, Not> {
-    beFoo: Not extends false ?
-      IsFoo<T> extends true ?
-        true
-      : `Expect \`${Stringify<T>}\` to be \`'foo'\`, but does not`
-    : IsFoo<T> extends false ? false
-    : `Expect the type not to be \`'foo'\`, but does`;
+declare module 'typroof/plugin' {
+  interface ValidatorRegistry {
+    beFoo: BeFooValidator;
   }
+}
+interface BeFooValidator extends Validator {
+  // Define the return type of your validator
+  return: IsNegated<this> extends false ?
+    IsFoo<Actual<this>> extends true ?
+      true
+    : `Expect \`${Stringify<Actual<this>>}\` to be \`'foo'\`, but does not`
+  : IsFoo<Actual<this>> extends true ? `Expect the type not to be \`'foo'\`, but does`
+  : false;
 }
 
 export {};
